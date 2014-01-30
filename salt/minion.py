a564c0af810b8cecc24562363968b06c82034caa
Michael Birtwell <michael.birtwell@gmail.com>
2014-01-30 17:09:39 +0000

    Fix pylint errors and delete commented code

--- salt/minion.py
+++ salt/minion.py
@@ -18,6 +18,7 @@ import time
 import traceback
 import sys
 import signal
+import errno
 from random import randint
 
 # Import third party libs
@@ -1160,7 +1161,6 @@ class Minion(object):
             )
 
         self.poller = zmq.Poller()
-        #self.epoller = zmq.Poller()
         self.socket = self.context.socket(zmq.SUB)
         self.socket.setsockopt(zmq.SUBSCRIBE, '')
         self.socket.setsockopt(zmq.IDENTITY, self.opts['id'])
@@ -1287,7 +1287,7 @@ class Minion(object):
                 # Check the event system
                 if socks.get(self.epull_sock) == zmq.POLLIN:
                     package = self.epull_sock.recv(zmq.NOBLOCK)
-                    log.debug("Handling event %s" % package)
+                    log.debug("Handling event %s", package)
                     try:
                         if package.startswith('module_refresh'):
                             self.module_refresh()
