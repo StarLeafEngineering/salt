a8ad4ffcdd997fe99e92d66d61a3b7c851fb7706
Michael Birtwell <michael.birtwell@gmail.com>
2014-07-16 14:02:26 +0100

    Add support for state.top function to salt.state state

--- salt/states/saltmod.py
+++ salt/states/saltmod.py
@@ -51,6 +51,7 @@ def state(
         ret='',
         highstate=None,
         sls=None,
+        top=None,
         env=None,
         test=False,
         fail_minions='',
@@ -75,6 +76,10 @@ def state(
         Defaults to None, if set to True the target systems will ignore any
         sls references specified in the sls option and call state.highstate
         on the targeted minions
+
+    top
+        Should be the name of a top file. If set state.top is called with this
+        top file instead of state.sls.
 
     sls
         A group of sls files to execute. This can be defined as a single string
@@ -116,6 +121,9 @@ def state(
     cmd_kw['ssh'] = ssh
     if highstate:
         fun = 'state.highstate'
+    elif top:
+        fun = 'state.top'
+        cmd_kw['arg'].append(top)
     elif sls:
         fun = 'state.sls'
         if isinstance(sls, list):
